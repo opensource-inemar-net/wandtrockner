e6c516bdcea348d2cee7be1898c281c5212e2fc2
Oliver <opensource@inemar.net>
2022-12-20 20:46:24 +0100

    install script

---
+++ script/install.sh
@@ -0,0 +1,11 @@
+sudo apt-get -y install s6
+sudo apt-get -y install hostapd dnsmasq
+
+
+pip install flask
+pip install python-gsmmodem-new
+pip install pathvalidate
+pip install pyModbusTCP
+
+
+chmod +x /usr/local/wandtrockner/script/*
