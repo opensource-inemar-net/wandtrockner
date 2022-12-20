c7e7fb37ee75bc368933ee76215612a99fe2fcfe
Schnabl,Oliver <Oliver.Schnabl@drei.com>
2022-12-20 20:40:59 +0100

    initial setup script

---
+++ script/dactspot.sh
@@ -0,0 +1,6 @@
+sudo sed -i '/denyinterfaces wlan0/d' /etc/dhcpcd.conf
+sudo rm /etc/network/interfaces.d/wlan0
+sudo sed -i 's/DAEMON_CONF/#DAEMON_CONF/g' /etc/default/hostapd
+sudo rm /etc/dnsmasq.conf
+sudo reboot
+
