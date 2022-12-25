cb7aefd9bc925aeb32bf696a66a1813d0bfc1903
inemar <opensource@inemar.net>
2022-12-25 18:36:16 +0100

    fix problem with path

--- version/V20221225-1200/script/actspot.sh
+++ version/V20221225-1200/script/actspot.sh
@@ -1,7 +1,7 @@
-sudo chown root:root /usr/local/wandtrockner/script/root
+#sudo chown root:root /usr/local/wandtrockner/active/active/script/root/*
 sudo echo 'denyinterfaces wlan0' >> /etc/dhcpcd.conf
-sudo cp  /usr/local/wandtrockner/script/root/wlan0 /etc/network/interfaces.d 
-sudo cp /usr/local/wandtrockner/script/root/hostapd /etc/default/hostapd
-sudo cp  /usr/local/wandtrockner/script/root/dnsmasq.conf /etc
+sudo cp /usr/local/wandtrockner/active/active/script/root/wlan0 /etc/network/interfaces.d 
+sudo cp /usr/local/wandtrockner/active/active/script/root/hostapd /etc/default/hostapd
+sudo cp /usr/local/wandtrockner/active/active/script/root/dnsmasq.conf /etc
 sudo reboot
 
