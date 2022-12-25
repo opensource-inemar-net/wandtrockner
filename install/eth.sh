345357244143e60537ea95f26fb984232b80ba5d
inemar <opensource@inemar.net>
2022-12-25 19:00:36 +0100

    fix ethernet config

--- install/eth.sh
+++ install/eth.sh
@@ -3,5 +3,9 @@ echo ""
 echo "*********************************"
 echo "configure static eth0 address"
 echo ""
-sudo cp /usr/local/wandtrockner/install/eth0 /etc/network/interfaces.d 
+sudo sed -i '/denyinterface eth0/d' /etc/dhcpcd.conf
+sudo echo 'denyinterface eth0' >> /etc/dhcpcd.conf
+sudo cp /usr/local/wandtrockner/install/eth0 /etc/network/interfaces.d
+sudo ifdown eth0
+sudo ifup eth0
 
