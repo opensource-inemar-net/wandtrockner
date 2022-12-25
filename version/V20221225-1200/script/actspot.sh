a45c5f8b44b5ecef06ff418d82a71e3cfa6035c3
inemar <opensource@inemar.net>
2022-12-25 21:10:44 +0100

    problem with the service setup

--- version/V20221225-1200/script/actspot.sh
+++ version/V20221225-1200/script/actspot.sh
@@ -3,5 +3,7 @@ sudo echo 'denyinterfaces wlan0' >> /etc/dhcpcd.conf
 sudo cp /usr/local/wandtrockner/active/active/script/root/wlan0 /etc/network/interfaces.d 
 sudo cp /usr/local/wandtrockner/active/active/script/root/hostapd /etc/default/hostapd
 sudo cp /usr/local/wandtrockner/active/active/script/root/dnsmasq.conf /etc
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
 sudo reboot
 
